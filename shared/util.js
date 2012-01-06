5c08f3cff3e7b3b0f911a8939cd1478ab4b9d1fd
Oz <dreadpirate@lifewanted.com>
2012-01-06 15:53:44 -0800

    Added util.parseQueryString method.

--- shared/util.js
+++ shared/util.js
@@ -1,5 +1,6 @@
 /// Library of utility functions for general usage.
 var util = (function(){
+    "use strict";
     var util = {};
 
     /// Detects if the given object is defined and not null.
@@ -11,5 +12,20 @@ var util = (function(){
         return Boolean( a !== undefined && a !== null );
     };
 
+    /// Parses a query string into an mapping of name/value pairs.
+    ///
+    /// @param {String} queryStr The query string to parse.
+    ///
+    /// @return {Object} The parsed query string.
+    util.parseQueryString = function( queryStr ){
+        var parts  = queryStr.split( '&' );
+        var params = {};
+        for( var i in parts ){
+            var nameValue = parts[i].split( '=' );
+            params[ nameValue[0] ] = nameValue[1];
+        }
+        return params;
+    };
+
     return util;
-});
+})();
