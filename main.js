50bc662593d96849352ffc1e974d87482fbdf9f1
Oz <dreadpirate@lifewanted.com>
2012-01-06 14:32:30 -0800

    Added api.X virtual host as well.

--- main.js
+++ main.js
@@ -1,29 +1,56 @@
 
-var servers = {};
+var server = null;
 (function(){
     "use strict";
-    var connect = require( 'connect' );
+
+    // Import the connect framework.
+    var connect  = require( 'connect' );
     connect.gzip = require( 'connect-gzip' ).gzip;
 
+    // Load the configuration.
+    // TODO: Make this an external config file.
     var hosts = [
         'nate.lifewanted.com'
     ];
 
+    var virtualServers = [];
     for( var i in hosts ){
-        var hostName = hosts[i];
-        var hostDir  = __dirname + '/' + hostName;
-        servers[hostName] = connect(
+        var hostName  = hosts[i];
+        var hostDir   = __dirname + '/' + hostName;
+        var apiName   = 'api.' + hostName;
+        var api       = require( hostDir + '/api.js' );
+        var shared    = __dirname + '/shared';
+        var logFormat = 'short';
+        virtualServers.push(
+            // Build the web host and link the API at /api.
             connect.vhost(
                 hostName,
                 connect(
                     connect.favicon(),
-                    connect.logger(),
+                    connect.logger( logFormat ),
                     connect.gzip(),
                     connect.static( hostDir + '/static' ),
-                    require( hostDir + '/api.js' )()
+                    connect.static( shared ),
+                    connect.bodyParser(),
+                    api( '/api' )
+                )
+            ),
+
+            // Next build the API server at its own location for external use.
+            connect.vhost(
+                apiName,
+                connect(
+                    connect.logger( logFormat ),
+                    connect.gzip(),
+                    connect.bodyParser(),
+                    api( '/' )
                 )
             )
-        ).listen( 80 );
+        );
+
+        // Now actually create the servers and bind it to port 80.
+        server = connect.apply( this, virtualServers );
+        server.listen( 80 );
     }
 })();
 
