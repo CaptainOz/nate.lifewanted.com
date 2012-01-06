00bef1c31aed54ea9757ba7629eb26c38e935a8f
Oz <dreadpirate@lifewanted.com>
2012-01-06 15:33:57 -0800

    Added util.exists

--- shared/util.js
+++ shared/util.js
@@ -2,5 +2,14 @@
 var util = (function(){
     var util = {};
 
+    /// Detects if the given object is defined and not null.
+    ///
+    /// @param {*} a The object to test.
+    ///
+    /// @return {Boolean} True if the object is defined and not null.
+    util.exists = function( a ){
+        return Boolean( a !== undefined && a !== null );
+    };
+
     return util;
 });
